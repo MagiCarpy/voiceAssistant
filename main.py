2342b8f174f4bfbc417f25f2c272036c1a9c2e23
MagiCarpy <brandon@digiduet.com>
2025-07-28 19:51:26 -0700

    improved data flow in main and model.py display_and_speak method. Oriented towards accepting conversation interruptions

--- main.py
+++ main.py
@@ -2,28 +2,25 @@ from models.transcriber import Transcriber
 from models.model import VoiceAssistant
 from processors.display import overlay_display
 import torch
-from multiprocessing import Process, Queue
-
-#torch.cuda.empty_cache()
+from multiprocessing import Process, Queue, set_start_method
 
 assistantName = "Marvin"
 
-if __name__ == "__main__":
+def main():
     assistant = VoiceAssistant()
     transcriber = Transcriber()
 
-    stop_signal = False
-    q = Queue()
-    q.put("")
-    p = Process(target=overlay_display, args=(q,))
+    overlay_queue = Queue()
+    overlay_queue.put("")
+    p = Process(target=overlay_display, args=(overlay_queue,))
     p.start()
-    while True:
 
+    while True:
         prev_audio, stream = transcriber.detect_wake(debug=True)
         print("\n<-----------VOICE ASSISTANT READY-----------> \n")
         if not p or not p.is_alive():
             print("Overlay starting")
-            p = Process(target=overlay_display, args=(q,))
+            p = Process(target=overlay_display, args=(overlay_queue,))
             p.start()
         query = transcriber.record_audio(prev_audio=prev_audio, stream=stream)
         print("\n<----------------PROCESSING----------------> \n")
@@ -31,13 +28,27 @@ if __name__ == "__main__":
             query = query[query.index(assistantName):]
             stream.close()
         except ValueError:
+            pass
+        except AttributeError:
             stream.close()
             print("\n<----------------Try Again----------------> \n") 
             continue
-        q.put("...")
+
+        overlay_queue.put("...")
         print(f"Query: {query} \n")
         print("Response: ", end="")
-        assistant.display_and_speak(query, overlay_queue=q)
+        assistant.display_and_speak(query, overlay_queue=overlay_queue)
         print("\n")
         print("\n<-----------------COMPLETE----------------->")
-        #display_response(get_model_response(query))
+
+def overlay_and_speak_worker():#assistant=VoiceAssistant(), transcriber=Transcriber()):
+    while True:
+        #check if wake word detected.
+            #interrupt and prompt model
+
+        #prev_audio, stream = transcriber.detect_wake(debug=True)
+        pass
+
+if __name__ == "__main__":
+    torch.cuda.empty_cache()
+    main()
